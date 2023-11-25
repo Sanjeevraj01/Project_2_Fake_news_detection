a1116235e9e80997012922927202888192eb9a55
Sanjeevraj <83357990+Sanjeevraj01@users.noreply.github.com>
2023-11-25 14:28:53 +0530

    Update and rename app.py to streamlit_app.py

--- app.py
+++
@@ -1,50 +0,0 @@
-# import libraries 
-import pandas as pd
-import streamlit as st 
-import pickle 
-from pathlib import Path
-
-# load_lr = pickle.load(open('lr_classifier.sav',"rb"))
-
-# vectorize = pickle.load(open('lr_vectorizer.sav', "rb"))
-
-load = pickle.load(open('lr_classifier.sav',"rb"))
-
-vectorize = pickle.load(open('lr_vectorizer.sav', "rb"))
-
-def news_prediction(news):
-  # new_news = "LONDON (Reuters) - LexisNexis, a provider of l."
-  testing_news = {"text":[news]}
-  new_def_test = pd.DataFrame(testing_news)
-  new_x_test = new_def_test['text']
-  new_tfidf_test = vectorize.transform(new_x_test)
-  pred_dt = load.predict(new_tfidf_test)
-  
-  if (pred_dt[0] == 0):
-    return "This is Fake News!"
-  else:
-    return "The News seems to be True!"
-
-
-    
-def main():
-  
-  st.title("Fake News Prediction System")
-  st.write("""## Input your News Article down below: """)
-  user_text = st.text_input('')
-  if st.button("Predict"):
-    news_pred = news_prediction(user_text)
-    
-    if (news_pred == "This is Fake News!"):
-      st.error(news_pred, icon="🚨")
-    else:
-      st.success(news_pred)
-      st.balloons()
-
-  
-if __name__ == "__main__":
-  main()
-  
-# fake data : 'House Intelligence Committee Chairman Devin Nunes is going to have a bad day. He s been under the assumption, like many of us, that the Christopher Steele-dossier was what prompted the Russia investigation so he s been lashing out at the Department of Justice and the FBI in order to protect Trump. As it happens, the dossier is not what started the investigation, according to documents obtained by the New York Times.Former Trump campaign adviser George Papadopoulos was drunk in a wine bar when he revealed knowledge of Russian opposition research on Hillary Clinton.On top of that, Papadopoulos wasn t just a covfefe boy for Trump, as his administration has alleged. He had a much larger role, but none so damning as being a drunken fool in a wine bar. Coffee boys  don t help to arrange a New York meeting between Trump and President Abdel Fattah el-Sisi of Egypt two months before the election. It was known before that the former aide set up meetings with world leaders for Trump, but team Trump ran with him being merely a coffee boy.In May 2016, Papadopoulos revealed to Australian diplomat Alexander Downer that Russian officials were shopping around possible dirt on then-Democratic presidential nominee Hillary Clinton. Exactly how much Mr. Papadopoulos said that night at the Kensington Wine Rooms with the Australian, Alexander Downer, is unclear,  the report states.  But two months later, when leaked Democratic emails began appearing online, Australian officials passed the information about Mr. Papadopoulos to their American counterparts, according to four current and former American and foreign officials with direct knowledge of the Australians  role. Papadopoulos pleaded guilty to lying to the F.B.I. and is now a cooperating witness with Special Counsel Robert Mueller s team.This isn t a presidency. It s a badly scripted reality TV show.Photo by Win McNamee/Getty Images.'
-
-# True Data : 'WASHINGTON (Reuters) - Transgender people will be allowed for the first time to enlist in the U.S. military starting on Monday as ordered by federal courts, the Pentagon said on Friday, after President Donald Trumps administration decided not to appeal rulings that blocked his transgender ban. Two federal appeals courts, one in Washington and one in Virginia, last week rejected the administrations request to put on hold orders by lower court judges requiring the military to begin accepting transgender recruits on Jan. 1. A Justice Department official said the administration will not challenge those rulings. The Department of Defense has announced that it will be releasing an independent study of these issues in the coming weeks. So rather than litigate this interim appeal before that occurs, the administration has decided to wait for DODs study and will continue to defend the presidents lawful authority in District Court in the meantime, the official said, speaking on condition of anonymity. In September, the Pentagon said it had created a panel of senior officials to study how to implement a directive by Trump to prohibit transgender individuals from serving. The Defense Department has until Feb. 21 to submit a plan to Trump. Lawyers representing currently-serving transgender service members and aspiring recruits said they had expected the administration to appeal the rulings to the conservative-majority Supreme Court, but were hoping that would not happen. Pentagon spokeswoman Heather Babb said in a statement: As mandated by court order, the Department of Defense is prepared to begin accessing transgender applicants for military service Jan. 1. All applicants must meet all accession standards. Jennifer Levi, a lawyer with gay, lesbian and transgender advocacy group GLAD, called the decision not to appeal great news. Im hoping it means the government has come to see that there is no way to justify a ban and that its not good for the military or our country, Levi said. Both GLAD and the American Civil Liberties Union represent plaintiffs in the lawsuits filed against the administration. In a move that appealed to his hard-line conservative supporters, Trump announced in July that he would prohibit transgender people from serving in the military, reversing Democratic President Barack Obamas policy of accepting them. Trump said on Twitter at the time that the military cannot be burdened with the tremendous medical costs and disruption that transgender in the military would entail. Four federal judges - in Baltimore, Washington, D.C., Seattle and Riverside, California - have issued rulings blocking Trumps ban while legal challenges to the Republican presidents policy proceed. The judges said the ban would likely violate the right under the U.S. Constitution to equal protection under the law. The Pentagon on Dec. 8 issued guidelines to recruitment personnel in order to enlist transgender applicants by Jan. 1. The memo outlined medical requirements and specified how the applicants sex would be identified and even which undergarments they would wear. The Trump administration previously said in legal papers that the armed forces were not prepared to train thousands of personnel on the medical standards needed to process transgender applicants and might have to accept some individuals who are not medically fit for service. The Obama administration had set a deadline of July 1, 2017, to begin accepting transgender recruits. But Trumps defense secretary, James Mattis, postponed that date to Jan. 1, 2018, which the presidents ban then put off indefinitely. Trump has taken other steps aimed at rolling back transgender rights. In October, his administration said a federal law banning gender-based workplace discrimination does not protect transgender employees, reversing another Obama-era position. In February, Trump rescinded guidance issued by the Obama administration saying that public schools should allow transgender students to use the restroom that corresponds to their gender identity.'
